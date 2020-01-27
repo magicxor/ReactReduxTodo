0162389798852ff40f310df0b916db0e9ea0b72e
Ilia Burakov <burakov.ilia@gmail.com>
2020-01-27 23:08:08 +0800

    upd

---
+++ precache-manifest.eca1a98f312597f188425f2df899d5c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e3befde10b1c7fa2eb0242d64adaef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2c9133e82c504311ee02",
+    "url": "./static/css/2.eae286e1.chunk.css"
+  },
+  {
+    "revision": "b702f1b330f0552a6d01",
+    "url": "./static/css/main.acd74306.chunk.css"
+  },
+  {
+    "revision": "2c9133e82c504311ee02",
+    "url": "./static/js/2.a96a6ae9.chunk.js"
+  },
+  {
+    "revision": "baa6ef6b5c80a1fb298345df580a1b27",
+    "url": "./static/js/2.a96a6ae9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b702f1b330f0552a6d01",
+    "url": "./static/js/main.8295fb1f.chunk.js"
+  },
+  {
+    "revision": "759dff4d510500762207",
+    "url": "./static/js/runtime-main.54a2de3a.js"
+  }
+]);
